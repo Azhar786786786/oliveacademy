e9b6ae7e2982c1934df1470597a98d21c4c87d0c
Azhar786786786 <talkazhar786@hotmail.com>
2025-12-14 06:30:49 +0530

    change resources

--- src/App.js
+++ src/App.js
@@ -1,12 +1,80 @@
 import logo from './logo.svg';
 import './App.css';
+import WildLife from './Components/WildLife';
+import JsCode from './Components/JsCode';
+import EventCondition from './Components/EventCondition';
+import MultipleCondition from './Components/MultipleCondition';
+import InputData from './Components/InputData';
+
+import PropsInReact from './Components/PropsInReact';
+import PropsMultipleDataInReact from './Components/PropsMultipleDataInReact';
+import PropsObjectInReact from './Components/PropsObjectInReact';
+import PropsMultipleObjectInReact from './Components/PropsMultipleObjectInReact';
+import PropsArrayInReact from './Components/PropsArrayInReact';
+
 
 function App() {
+
+  const empName = "Azhar Hussain Khan";
+  const empAge = 35;
+  const addr = "037, chilmapur, Rustampur dhala, near jama masjid, shivepuri new colony 273016, U.P. (India).";
+  
+  let employee = {
+    name: "Shaista khatoon",
+    age: 31,
+    salary: 33000,
+    emailId: "talkshaista786@gmail.com"
+  }
+  
+  let employee1 = {
+    name: "Salma khatoon",
+    age: 33,
+    salary: 35000,
+    emailId: "talksalma786@gmail.com"
+  }
+  let employee2 = {
+    name: "Shaista khatoon",
+    age: 31,
+    salary: 33000,
+    emailId: "talkshaista786@gmail.com"
+  }
+  let employee3 = {
+    name: "Nazma khatoon",
+    age: 35,
+    salary: 39000,
+    emailId: "talknazma786@gmail.com"
+  }
+  let employee4 = {
+    name: "Azhar Hussain Khan",
+    age: 38,
+    salary: 46000,
+    emailId: "talkazhar786@hotmail.com"
+  }
+  
+  let university=["DDU","Munad","ITM","IIT","JNU","AU"];
+  
+
   return (
     <div className="App">
 
       <img src={logo} className="App-logo" alt="logo" />
       <h1>This is created by Azhar Hussain Khan</h1>
+
+      <WildLife />
+    <JsCode />
+    <EventCondition />
+    <MultipleCondition />
+    <InputData />
+    <PropsInReact empName="Azhar Hussain Khan" />
+    <PropsMultipleDataInReact empName={empName} empAge={empAge} addr={addr} />
+    <PropsObjectInReact user={employee} />
+
+    <PropsMultipleObjectInReact user={employee1} />
+    <PropsMultipleObjectInReact user={employee2} />
+    <PropsMultipleObjectInReact user={employee3} />
+    <PropsMultipleObjectInReact user={employee4} />
+
+    <PropsArrayInReact uni={university} />
 
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,75 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import WildLife from './Components/WildLife';
-import JsCode from './Components/JsCode';
-import EventCondition from './Components/EventCondition';
-import MultipleCondition from './Components/MultipleCondition';
-import InputData from './Components/InputData';
 import reportWebVitals from './reportWebVitals';
-import PropsInReact from './Components/PropsInReact';
-import PropsMultipleDataInReact from './Components/PropsMultipleDataInReact';
-import PropsObjectInReact from './Components/PropsObjectInReact';
-import PropsMultipleObjectInReact from './Components/PropsMultipleObjectInReact';
-import PropsArrayInReact from './Components/PropsArrayInReact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const empName = "Azhar Hussain Khan";
-const empAge = 35;
-const addr = "037, chilmapur, Rustampur dhala, near jama masjid, shivepuri new colony 273016, U.P. (India).";
-
-let employee = {
-  name: "Shaista khatoon",
-  age: 31,
-  salary: 33000,
-  emailId: "talkshaista786@gmail.com"
-}
-
-let employee1 = {
-  name: "Salma khatoon",
-  age: 33,
-  salary: 35000,
-  emailId: "talksalma786@gmail.com"
-}
-let employee2 = {
-  name: "Shaista khatoon",
-  age: 31,
-  salary: 33000,
-  emailId: "talkshaista786@gmail.com"
-}
-let employee3 = {
-  name: "Nazma khatoon",
-  age: 35,
-  salary: 39000,
-  emailId: "talknazma786@gmail.com"
-}
-let employee4 = {
-  name: "Azhar Hussain Khan",
-  age: 38,
-  salary: 46000,
-  emailId: "talkazhar786@hotmail.com"
-}
-
-let university=["DDU","Munad","ITM","IIT","JNU","AU"];
 
 root.render(
   <React.StrictMode>
     <App />
-    <WildLife />
-    <JsCode />
-    <EventCondition />
-    <MultipleCondition />
-    <InputData />
-    <PropsInReact empName="Azhar Hussain Khan" />
-    <PropsMultipleDataInReact empName={empName} empAge={empAge} addr={addr} />
-    <PropsObjectInReact user={employee} />
-
-    <PropsMultipleObjectInReact user={employee1} />
-    <PropsMultipleObjectInReact user={employee2} />
-    <PropsMultipleObjectInReact user={employee3} />
-    <PropsMultipleObjectInReact user={employee4} />
-
-    <PropsArrayInReact uni={university} />
+    
   </React.StrictMode>
 );
 
